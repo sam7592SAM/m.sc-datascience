76f86f93d994b683e00adeae1b9f00c3364d7936
sam7592SAM <87634326+sam7592SAM@users.noreply.github.com>
2023-03-03 23:10:34 +0000

    Update as1.py

--- as1.py
+++ as1.py
@@ -27,27 +27,40 @@ print(mlsusb)
 
 
 def line_gr(lin1, con, lbl):
+    ''' Function to create lineplot. Arguements such as lin1, con, and lb1
+    are passed into line_gr. 'lin1' argument is used for get the values from
+    the dataframe 'mlsus', 'con' arguement is used to get the country lists
+    from the dataframe and 'lb1' is an arguement to get the labels that needed
+    to plot on the graph.'''
     return plt.plot(lin1["Year"], lin1[con], label=lbl)
 
 # 2. Pie Graph
 
 
-def pie_gr(p1, lb2):
+def pie_gr(p1):
+    ''' Function to create pie chart. Arguement 'p1' is
+    passed into pie_gr and is used to collect the values from the dataframe
+    'mlsusp'.'''
     plt.figure()
     plt.pie(p1['India'], labels=p1.index, autopct='%1.f%%')
     return
+
 
 # 3 Bar Graph
 
 
 def bar_gr(b1, contr, cas):
+    '''Function to create Bar Graph. Arguements such as b1, contr, cas
+    are passed into bar_gr. 'b1' arguement is used to get the values from the
+    dataframe 'mlsusb', 'contr' and 'cas' are used to get the values of the
+    countries to plot in X-axis and Y-axis respectively.  '''
     plt.figure(figsize=[10, 6])
     plt.bar(b1[contr], b1[cas])
     return
 
 
-# Caliing functions
-# 1. Calling Line Graph using line_gr
+# Calling functions
+# 1. Inserting values into the Line Graph using line_gr
 
 line_gr(mlsus, "India", "India")
 line_gr(mlsus, "Democratic Republic of the Congo", "D_Rep_Congo")
@@ -55,21 +68,23 @@ line_gr(mlsus, "Nigeria", "Nigeria")
 line_gr(mlsus, "Uganda", "Uganda")
 line_gr(mlsus, "Ghana", "Ghana")
 
+# Labelling X and Y plots
 plt.xlabel("Year")
 plt.ylabel("No of Cases")
 plt.title("Suspected Malaria Cases")
 plt.savefig("linegr.png")
 plt.legend()
 
-# 2. Calling Pie Graph using pie_gr
+# 2. Retrieveing the values from the dataframe into the Pie Graph using pie_gr
 
-pie_gr(mlsusp, ["2015", "2016", "2017", "2018", "2019", "2020"])
+pie_gr(mlsusp)
 
 plt.title("Malaria Spread in India")
-plt.savefig("piegr.png")
+plt.savefig("piegrin.png")
 plt.show()
 
-# 3. Calling Bar Graph using bar_gr
+
+# 3. Inserting values into the Bar Graph using bar_gr
 
 bar_gr(mlsusb, "Countries", "Suspected Cases")
 
